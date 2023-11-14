f656a390824943e61a3abea309ae99d0f9f6d429
ccap86 <85033044+ccap86@users.noreply.github.com>
2023-11-14 06:59:03 -0600

    Refractor - GET

--- CORPCOMMENT/script.js
+++ CORPCOMMENT/script.js
@@ -1,20 +1,53 @@
 // -- GLOBAL --
+const MAX_CHARS = 150;
+const BASE_API_URL = 'https://bytegrad.com/course-assets/js/1/api';
+
+
 const textareaEl = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
 const formEl = document.querySelector('.form');
 const feedbackListEl = document.querySelector('.feedbacks');
+const submitBtnEl = document.querySelector('.submit-btn');
+const spinnerEl = document.querySelector('.spinner');
+
+const renderFeedbackItem = feedbackItem => {
+    // new feedback item html
+    const feedbackItemHTML = `
+    <li class="feedback">
+        <button class="upvote">
+            <i class="fa-solid fa-caret-up upvote__icon"></i>
+            <span class="upvote__count">${feedbackItem.upvoteCount}</span>
+        </button>
+        <section class="feedback__badge">
+            <p class="feedback__letter">${feedbackItem.badgeLetter}</p>
+        </section>
+        <div class="feedback__content">
+            <p class="feedback__company">${feedbackItem.company}</p>
+            <p class="feedback__text">${feedbackItem.text}</p>
+        </div>
+        <p class="feedback__date">${feedbackItem.daysAgo === 0 ? "NEW": `${feedbackItem.daysAgo}d`}</p>
+    </li>
+    `;
+
+    // insert new feedback item in list
+    feedbackListEl.insertAdjacentHTML("beforeend", feedbackItemHTML);
+
+}
+
+
 
 // --- counter component
 
 const inputHandler = () => {
     // determine max number characters
-    const maxNrChars = 150;
+    const maxNrChars = MAX_CHARS;
 
     // determine number of characters currently typed
     const nrCharsTyped = textareaEl.value.length;
     
     // calculate number of characters left (max - currently typed)
     const charsLeft = maxNrChars - nrCharsTyped;
+    
     
     // show number of characters left
     counterEl.textContent = charsLeft;
@@ -27,31 +60,29 @@ textareaEl.addEventListener('input', inputHandler);
 //-- submit component --
 
 // const submitHandler = (event) => { -- can remove () since only one input
-const submitHandler = (event) => {
+const submitHandler = event => {
     // prevent default browser action (submitting data to 'action' --address and loading new page)
     event.preventDefault();
     
     // get text from textarea
     const text = textareaEl.value;  
-    
+
+    validate = formValidation => {
+        formEl.classList.add(formValidation);
+
+        // remove valid indicator after 2 seconds
+        setTimeout(() => {
+            formEl.classList.remove(formValidation);
+        },2000);
+    };
     // validate text (e.g. check if hashtag is present and text is long enough)
     if( text.includes('#') && text.length >= 5) {
         // show valid indicator
-        formEl.classList.add('form--valid');
-
-        // remove valid indicator after 2 seconds
-        setTimeout(() =>{
-            formEl.classList.remove('form--valid');
-        },2000);
+        validate('form--valid');
     } else {
         // show invalid indicator
-        formEl.classList.add('form--invalid');
-
-        // remove invalid indicator after 2 seconds
-        setTimeout(() =>{
-            formEl.classList.remove('form--invalid');
-        },2000);
-
+        validate('form--invalid');
+        
         // focus textarea
         textareaEl.focus();
 
@@ -62,38 +93,66 @@ const submitHandler = (event) => {
     // we have text, now extract other info
     const hashtag = text.split(' ').find(el =>el.includes('#'));
     const company= hashtag.substring(1);
-    //const company=hashtag.replace('#','');
-     
+     //const company=hashtag.replace('#',''); 
+
     const badgeLetter = company.substring(0,1).toUpperCase();
     //console.log(company.charAt(0).toUpperCase());
 
-    
-    const upVoteCount = 0;
+    const upvoteCount = 0;
     const daysAgo = 0;
+    
+    // render feedback item in list
+    const feedbackItem = {
+        upvoteCount: upvoteCount,
+        company: company,
+        badgeLetter: badgeLetter,
+        daysAgo: daysAgo,
+        text: text
+    };
+    renderFeedbackItem(feedbackItem);
 
-    // new feedback item html
-    const feedbackItemHTML = `
-    <li class="feedback">
-        <button class="upvote">
-            <i class="fa-solid fa-caret-up upvote__icon"></i>
-            <span class="upvote__count">${upVoteCount}</span>
-        </button>
-        <section class="feedback__badge">
-            <p class="feedback__letter">${badgeLetter}</p>
-        </section>
-        <div class="feedback__content">
-            <p class="feedback__company">${company}</p>
-            <p class="feedback__text">${text}</p>
-        </div>
-        <p class="feedback__date">${daysAgo === 0 ? "NEW": `${daysAgo}d`}</p>
-    </li>
-    `;
+    // send feedback item to list
+    fetch(`${BASE_API_URL}/feedbacks`, {
+        method: 'POST',
+        body: JSON.stringify(feedbackItem),
+        headers: {
+            Accept: 'application/json',
+            'content-type': 'application/json'
+        }
+    })
+    .then(response => {
+        if(!response.ok){
+            return console.log('Something went wrong');
+        }
+            console.log('Successfully submitted');
+    })
+    .catch(error => console.log(error));
 
-    // insert new feedback item in list
-    feedbackListEl.insertAdjacentHTML("beforeend", feedbackItemHTML);
+    // clear textarea
+    textareaEl.value = '';
 
+    // set submit button to blur
+    submitBtnEl.blur();
 
+    // reset the counter
+    counterEl.textContent = MAX_CHARS;
 }
 
 formEl.addEventListener('submit', submitHandler);
+
+// -- Feedback List Component --
+fetch(`${BASE_API_URL}/feedbacks`)
+.then(response => response.json())
+.then(data => {
+    // remove spinner element
+    //spinnerEl.classList.remove("spinner");    
+    spinnerEl.remove();    
+
+    // iterate over each element in the feedbacks array and render it in the list
+    data.feedbacks.forEach(feedbackItem => renderFeedbackItem(feedbackItem));
+})
+.catch(error => {
+    feedbackListEl.textContent = `Failed to fetch feedback item. Error message: ${error.message}`;
+});
+
 
