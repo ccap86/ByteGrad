7ddc9c63f753a3c905632e29b7cfe2772a97d210
ccap86 <85033044+ccap86@users.noreply.github.com>
2023-12-29 09:26:43 -0600

    Moved jobdetails rendering and separated out jobList component into own file

--- RMTDEV/index.js
+++ RMTDEV/index.js
@@ -1,5 +1,6 @@
 import './src/components/Error.js';
 import './src/components/jobList.js';
+import './src/components/jobDetails.js';
 import './src/components/Search.js';
 import './src/components/Spinner.js';
 
