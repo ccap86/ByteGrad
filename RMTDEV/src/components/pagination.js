bf405c809cc52e422e4a39ae43534d8d9a0bbf55
ccap86 <85033044+ccap86@users.noreply.github.com>
2024-01-17 07:27:22 -0600

    Imported html elements into pagination.js

---
+++ RMTDEV/src/components/pagination.js
@@ -0,0 +1,10 @@
+import {
+    state,
+    paginationEl,
+    paginationBtnNextEl,
+    paginationBtnBackEl,
+    paginationNumberBackEl,
+    paginationNumberNextEl,
+
+} from '../common.js';
+import renderJobList from './jobList.js';
