0edab3eb9a5e86d01ca3e8c2f6bc2e67e2ea7220
ccap86 <85033044+ccap86@users.noreply.github.com>
2024-01-17 07:09:21 -0600

    Starting Pagination Component

--- RMTDEV/index.js
+++ RMTDEV/index.js
@@ -1,6 +1,7 @@
 import './src/components/Error.js';
 import './src/components/jobList.js';
 import './src/components/JobDetails.js';
+import './src/components/pagination.js';
 import './src/components/Search.js';
 import './src/components/Sorting.js';
 import './src/components/Spinner.js';
