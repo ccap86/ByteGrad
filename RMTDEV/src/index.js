6cdf87ad0620146bea05d9d179e88f42c5ddc30d
ccap86 <85033044+ccap86@users.noreply.github.com>
2024-05-18 08:13:49 -0500

    Implemented Webpack

---
+++ RMTDEV/src/index.js
@@ -0,0 +1,12 @@
+import './components/Bookmarks.js';
+import './components/Error.js';
+import './components/jobList.js';
+import './components/JobDetails.js';
+import './components/pagination.js';
+import './components/Router.js';
+import './components/Search.js';
+import './components/Sorting.js';
+import './components/Spinner.js';
+import './components/Storage.js';
+
+import './index.css';
