b3f7d4fd44e78afef67443fc3bd6505a4d504053
ccap86 <85033044+ccap86@users.noreply.github.com>
2024-06-28 16:33:23 -0500

    Project Complete

--- fancy-counter-vite/src/CountButton.jsx
+++ fancy-counter-vite/src/CountButton.jsx
@@ -1,7 +1,7 @@
 import { PlusIcon, MinusIcon } from "@radix-ui/react-icons";
 
-export default function CountButton({ type, setCount }) {
-  const handleClick = () => {
+export default function CountButton({ type, setCount, locked }) {
+  const handleClick = (event) => {
     setCount((prev) => {
       if (type === "minus") {
         //   my code to fix limit
@@ -18,12 +18,19 @@ export default function CountButton({ type, setCount }) {
         }
         return newCount;
       } else {
-        return prev + 1;
+        // return prev + 1;
+        const newCount = prev + 1;
+        if (newCount > 5) {
+          return 5;
+        }
+
+        return newCount;
       }
     });
+    event.currentTarget.blur();
   };
   return (
-    <button onClick={handleClick} className="count-btn">
+    <button disabled={locked} onClick={handleClick} className="count-btn">
       {type === "minus" ? (
         <MinusIcon className="count-btn-icon" />
       ) : (
--- fancy-counter-vite/src/Title.jsx
+++ fancy-counter-vite/src/Title.jsx
@@ -1,6 +1,16 @@
-export default function Title() {
+export default function Title({ locked }) {
   //return <h1 className="title">Fancy Counter</h1>;
-  return <h1 className="title">Fancy Counter</h1>;
+  return (
+    <h1 className="title">
+      {locked ? (
+        <span>
+          LIMIT REACHED BUY <b>PRO</b> &gt;5
+        </span>
+      ) : (
+        "Fancy Counter"
+      )}
+    </h1>
+  );
 }
 
 // export default Title;
